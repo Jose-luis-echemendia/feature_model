dfbd752d416e0eaea2ae775bf07c7463b184c850
Jose-luis-echemendia <echemendiajoseluis@gmail.com>
2025-09-16 17:52:45 -0400

    creando archivos y carpetas

--- backend/app/api/v1/router.py
+++ backend/app/api/v1/router.py
@@ -1,15 +1,16 @@
+from backend.app.api.v1.endpoints import item, user
 from fastapi import APIRouter
 
 from app.core.config import settings
-from app.api.v1.endpoints import items, login, private, users, utils, appointments
+from app.api.v1.endpoints import login, private, utils, appointments
 
 api_router = APIRouter()
 
 # Incluir cada router con su prefijo y etiquetas
 # Esto agrupará las rutas en la documentación de Swagger UI
 api_router.include_router(login.router, tags=["Login"])
-api_router.include_router(users.router, tags=["Users"])
-api_router.include_router(items.router, tags=["Items"])
+api_router.include_router(user.router, tags=["Users"])
+api_router.include_router(item.router, tags=["Items"])
 api_router.include_router(utils.router, tags=["Utils"]) # Root
 api_router.include_router(appointments.router, tags=["Appointments"]) #  prefix="/appointments",
 
