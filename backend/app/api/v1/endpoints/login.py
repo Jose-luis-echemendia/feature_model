e5086a08d9a0dc9dde730dfd2a0a526fe86b88ea
Jose-luis-echemendia <echemendiajoseluis@gmail.com>
2025-09-24 13:44:58 -0400

    ok

--- backend/app/api/v1/endpoints/login.py
+++ backend/app/api/v1/endpoints/login.py
@@ -3,15 +3,13 @@ from typing import Annotated, Any
 
 from fastapi import APIRouter, Depends, HTTPException
 from fastapi.responses import HTMLResponse
-from fastapi.security import OAuth2PasswordRequestForm
 
 from app import crud
 from app.api.deps import CurrentUser, SessionDep, get_current_active_superuser
 from app.core import security
 from app.core.config import settings
 from app.core.security import get_password_hash
-from app.models.common import Message, NewPassword, Token
-from app.models.user import UserPublic
+from app.models import Message, NewPassword, Token, LoginRequest, UserPublic
 from app.utils import (
     generate_password_reset_token,
     generate_reset_password_email,
@@ -23,14 +21,12 @@ router = APIRouter(tags=["login"])
 
 
 @router.post("/login/access-token/")
-def login_access_token(
-    session: SessionDep, form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
-) -> Token:
+def login_access_token(*, session: SessionDep, login_data: LoginRequest) -> Token:
     """
     OAuth2 compatible token login, get an access token for future requests
     """
     user = crud.authenticate(
-        session=session, email=form_data.email, password=form_data.password
+        session=session, email=login_data.email, password=login_data.password
     )
     if not user:
         raise HTTPException(status_code=400, detail="Incorrect email or password")
